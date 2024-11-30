09cdb49a4f5edf3ae7786f47665d41ef17699031
Crispy <chebunin19@gmail.com>
2024-11-30 10:58:59 +0300

    Add Gracefull Shutdown

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,4 +1,6 @@
 import asyncio
+from contextlib import suppress
+import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
@@ -10,12 +12,29 @@ from handlers import user_handlers
 from logger.logger import get_logger
 
 
+async def shutdown(bot: Bot, dp: Dispatcher, logger: logging.Logger) -> None:
+    """
+    Gracefully shutdown bot and resources.
+    """
+
+    logger.info("Shutting down bot...")
+
+    logger.debug("Closing storage...")
+    await dp.fsm.storage.close()
+
+    logger.debug("Closing bot...")
+    await bot.session.close()
+
+    logger.info("Bot shut down successfully.")
+
+
 async def main() -> None:
     # Loading the config
     config: Config = load_config()
 
     # Configuring the logging
     logger = get_logger("main", config.logger)
+    logger.info("Starting bot...")
 
     logger.debug("Initialising the storage object...")
     storage = MemoryStorage()
@@ -26,13 +45,21 @@ async def main() -> None:
     logger.debug("Registering routers...")
     dp.include_router(user_handlers.router)
 
+    # Graceful shutdown handling
     logger.info("Bot was started")
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    try:
+        await bot.delete_webhook(drop_pending_updates=True)
+
+        await dp.start_polling(bot)
+    except Exception as e:
+        logger.error("An error occurred: %s", e)
+    finally:
+        await shutdown(bot, dp, logger)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    with suppress(KeyboardInterrupt):
+        asyncio.run(main())
 
 
 __all__ = []
