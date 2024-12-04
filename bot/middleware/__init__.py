96e35f910e714c22eedf6c885070c6993744914b
Crispy <chebunin19@gmail.com>
2024-12-04 17:07:50 +0300

    Add LoggingMiddleware

--- bot/middleware/__init__.py
+++ bot/middleware/__init__.py
@@ -2,12 +2,14 @@ from logging import Logger
 
 from aiogram import Dispatcher
 
+from middleware.logging import LoggingMiddleware
 from middleware.user import ACLMiddleware
 from service import DefaultUserService
 
 
 def setup(dispatcher: Dispatcher, logger: Logger, user_service: DefaultUserService):
     dispatcher.update.middleware(ACLMiddleware(user_service=user_service))
+    dispatcher.update.middleware(LoggingMiddleware(logger))
 
 
 __all__ = ["setup"]
